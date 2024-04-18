f5933681dbe30d9b72bd67c272188a7dbeac581d
Leonardo Henrique <code.leoh@gmail.com>
2024-04-18 11:17:14 -0300

    Criação do cenário feito

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 const char = createMage('Leonardo');
 const monster = createBigMonster();
 
-console.log(monster.name);
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+);
